1677ead0bbdb3d222785afa881414e0f83433489
Tuanquan127 <ltquan20@clc.fitus.edu.vn>
2023-02-24 11:20:41 +0700

    add bottom navigation bar

---
+++ app/src/main/java/com/group7/gallerium/AllPhotoFragment.java
@@ -0,0 +1,33 @@
+package com.group7.gallerium;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AllPhotoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AllPhotoFragment extends Fragment {
+    public AllPhotoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_all_photo, container, false);
+    }
+}
