84c0d4e41c9078dc5393bb532f963f747f29de41
Minh Bao <bebaoboy@gmail.com>
2023-02-24 13:37:26 +0700

    add toolbar

--- app/src/main/java/com/group7/gallerium/PhotoFragment.java
+++ app/src/main/java/com/group7/gallerium/PhotoFragment.java
@@ -2,6 +2,7 @@ package com.group7.gallerium;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,11 +11,13 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AllPhotoFragment#newInstance} factory method to
+ * Use the {@link PhotoFragment#} factory method to
  * create an instance of this fragment.
  */
-public class AllPhotoFragment extends Fragment {
-    public AllPhotoFragment() {
+public class PhotoFragment extends Fragment {
+    private View view;
+    private Toolbar toolbar;
+    public PhotoFragment() {
         // Required empty public constructor
     }
 
@@ -27,7 +30,12 @@ public class AllPhotoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_photo, container, false);
+        toolbar = view.findViewById(R.id.toolbar_photo);
 
-        return inflater.inflate(R.layout.fragment_all_photo, container, false);
+        toolbar.inflateMenu(R.menu.menu_photo);
+        toolbar.setTitle(R.string.app_name);
+
+        return view;
     }
 }
