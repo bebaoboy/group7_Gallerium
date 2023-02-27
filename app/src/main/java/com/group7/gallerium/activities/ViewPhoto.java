b286623662f158d9ffbfb2cd3cc22b9afd097fea
Minh Bao <bebaoboy@gmail.com>
2023-02-27 21:49:09 +0700

    add view photo nav bar

--- app/src/main/java/com/group7/gallerium/activities/ViewPhoto.java
+++ app/src/main/java/com/group7/gallerium/activities/ViewPhoto.java
@@ -1,16 +1,38 @@
 package com.group7.gallerium.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.group7.gallerium.R;
 
 public class ViewPhoto extends AppCompatActivity {
+
+    BottomNavigationView bottom_nav;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_photo);
+        toolbar = findViewById(R.id.toolbar_view_photo); // toolbar is null ????
+        bottom_nav = findViewById(R.id.view_photo_bottom_navigation);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        // toolbar.inflateMenu(R.menu.menu_view_photo);
+//        toolbar.setTitle("View Photo");
+//        toolbar.setSubtitle("Ngày tháng năm");
+//        toolbar.setTitleTextAppearance(context, R.style.ToolbarTitle);
+        return super.onCreateView(name, context, attrs);
     }
 }
