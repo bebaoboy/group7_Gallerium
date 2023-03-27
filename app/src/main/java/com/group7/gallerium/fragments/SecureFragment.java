9e62e4a2dced81c96250832b24e7f38b1f8ae939
Tuanquan127 <ltquan20@clc.fitus.edu.vn>
2023-03-27 21:44:09 +0700

    add create/ save password and restore function to secure fragment

--- app/src/main/java/com/group7/gallerium/fragments/SecureFragment.java
+++ app/src/main/java/com/group7/gallerium/fragments/SecureFragment.java
@@ -2,8 +2,11 @@ package com.group7.gallerium.fragments;
 
 import android.animation.Animator;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +17,22 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.button.MaterialButton;
 import com.group7.gallerium.R;
+import com.group7.gallerium.utilities.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class SecureFragment extends Fragment {
     private View view;
@@ -30,15 +43,38 @@ public class SecureFragment extends Fragment {
     private EditText txtPass;
     private MaterialButton btnClear, btnEnter;
 
+    private ArrayList<String> paths;
+
     boolean isLandscape = false;
 
+    SharedPreferences sharedPreferences;
+
+    FileUtils fileUtils;
+    String password, question, answer;
+
+    private  String secretPath;
+
     public SecureFragment() {}
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         isLandscape = this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        fileUtils = new FileUtils();
+
+        sharedPreferences = requireContext().getSharedPreferences("secured_list", Context.MODE_PRIVATE);
+        password = sharedPreferences.getString("password", "");
+        question = sharedPreferences.getString("question", "");
+        answer = sharedPreferences.getString("answer", "");
+
+        secretPath = Environment.getExternalStorageState() + "/" + ".secret";
+
+        Log.d("answer", answer);
+        Log.d("question", question);
+        Log.d("password", password);
     }
+
 
     @Override
     public void onResume() {
@@ -70,11 +106,101 @@ public class SecureFragment extends Fragment {
         }
 
         btnClear.setOnClickListener((view1 -> txtPass.setText("")));
-        btnEnter.setOnClickListener((view1) -> Toast.makeText(this.getContext(), "Your pass: " + txtPass.getText(), Toast.LENGTH_SHORT).show());
+        btnEnter.setOnClickListener((view1) -> {
+            Toast.makeText(this.getContext(), "Your pass: " + txtPass.getText(), Toast.LENGTH_SHORT).show();
+            try {
+                verifiedLogic();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
         context = requireContext();
+
+        showViewLogic();
         toolbarSetting();
         return view;
     }
+
+    private void showViewLogic() {
+        if(txtPass.getText().toString().equals("")){
+            view.findViewById(R.id.secure_scrollview).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.main_secured_page).setVisibility(View.GONE);
+        }
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_questionaire, null);
+
+        builder.setView(dialogView)
+                .setPositiveButton(R.string.ok, (dialogInterface, i) -> {
+                    var editQuestion = (EditText)dialogView.findViewById(R.id.question);
+                    var editAnswer = (EditText)dialogView.findViewById(R.id.answer);
+                    SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                    myEdit.putString("question", editQuestion.getText().toString());
+                    myEdit.putString("answer", editAnswer.getText().toString());
+                    myEdit.apply();
+                    showViewLogic();
+                }).setNegativeButton(R.string.cancel, ((dialogInterface, i) -> {
+                     dialogInterface.dismiss();
+                }));
+        builder.create();
+        builder.show();
+    }
+
+    void verifiedLogic() throws FileNotFoundException {
+        if(txtPass.getText().length() < 4){
+            Toast.makeText(requireContext(), "Password not valid", Toast.LENGTH_SHORT).show();
+        }else if(txtPass.getText().toString().equals(password)) {
+            SharedPreferences.Editor myEdit = sharedPreferences.edit();
+            myEdit.putString("password", txtPass.getText().toString());
+            myEdit.apply();
+            if (!question.isBlank() && !answer.isBlank()) {
+                showAlertDialog();
+                createSecuredDir();
+            } else {
+                view.findViewById(R.id.secure_scrollview).setVisibility(View.GONE);
+                view.findViewById(R.id.main_secured_page).setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void createSecuredDir() throws FileNotFoundException {
+        File secureDir = new File(context.getFilesDir(), "secure-subfolder");
+        Log.d("context-Filedir", context.getFilesDir().getAbsolutePath());
+        if(!secureDir.exists()){
+            secureDir.mkdirs();
+            password = sharedPreferences.getString("password", "");
+        }
+
+        view.findViewById(R.id.secure_scrollview).setVisibility(View.GONE);
+        view.findViewById(R.id.main_secured_page).setVisibility(View.VISIBLE);
+        
+       // createSecuredFile(new File(secureDir, "hello.txt"), "");
+        getPaths(secureDir);
+    }
+
+    private void createSecuredFile(File fileName, String data) {
+        String fileContents = "Hello world!";
+        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            fos.write(fileContents.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ArrayList<String> getPaths(File file) throws FileNotFoundException {
+        File[] files = file.listFiles();
+        for(File f: files){
+            Log.d("file-path", f.getAbsolutePath());
+            FileInputStream fin = new FileInputStream(f);
+
+        }
+        return null;
+    }
+
 
     void toolbarSetting(){
         toolbar = view.findViewById(R.id.toolbar_secure);
